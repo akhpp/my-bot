f40850c3f619ca31883e3b761d78037b66d60c55
akhpp <74254758+akhpp@users.noreply.github.com>
2021-01-04 09:48:40 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+
+
+import os
+
+class Config:
+	
+ TOKEN=os.environ.get("BOT_TOKEN","1483431917:AAFQlp3UJeQq7GEqJzkV6S9Bcz3Rqh-ish8")
+
+ START_TEXT="Hi [{}](tg://user?id={})\nI am Anonymous File Sender Bot.Send /help To Know What I Can Do \n ©BathRecords"
+ HELP_TEXT="Forward Me A File,Video,Audio,Photo or Anything And \nI will Send You the File Back\n"
+	
